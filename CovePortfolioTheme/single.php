1f8d9205fbee55f5c46f9c212adaa14b12d6aeec
Colin Cove <Colin Cove>
2016-10-31 11:28:54 +0900

    fixing minor bug with single pages not including the resume URL defined in the theme

--- CovePortfolioTheme/single.php
+++ CovePortfolioTheme/single.php
@@ -2,5 +2,6 @@
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
+$context['resume'] = get_theme_mod('resume_pdf');
 Timber::render( 'templates/single-project.twig', $context );
 ?>
