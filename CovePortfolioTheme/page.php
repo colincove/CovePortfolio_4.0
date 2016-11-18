7709340eaa3f3efb463222e7d12318113278274f
Colin Cove <Colin Cove>
2016-11-17 23:11:15 -0800

    small css modifications

--- CovePortfolioTheme/page.php
+++ CovePortfolioTheme/page.php
@@ -6,7 +6,7 @@ $context['post'] = $post;
 $context['is_front_page'] = is_front_page();
 $context['resume'] = get_theme_mod('resume_pdf');
 if(is_front_page()){
-	$context['projects'] = Timber::get_posts(array( 'posts_per_page' => 20, 'category_name' => 'project' ));
+	$context['projects'] = Timber::get_posts(array( 'posts_per_page' => 20, 'category_name' => 'project', 'post_status' => 'public' ));
 }else{
 	$context['posts'] = Timber::get_posts(array( 'posts_per_page' => 20, 'category_name' => 'uncategorized' ));
 }
